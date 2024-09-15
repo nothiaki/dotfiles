fa89a5137cada959c8822c8f9ab38cfbdf4177be
nothiaki <celsohenrique367@gmail.com>
2024-09-15 20:42:27 -0300

    fix: nvim gruvbox style and polybar

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -5,6 +5,10 @@ return {
     vim.cmd([[
       colorscheme gruvbox
       hi SignColumn guibg=#282828
+      hi DiagnosticSignError guibg=#282828
+      hi DiagnosticSignWarn guibg=#282828
+      hi DiagnosticSignInfo guibg=#282828
+      hi DiagnosticSignHint guibg=#282828
     ]])
   end,
 }
