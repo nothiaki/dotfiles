3a5172d647691108253dada71ee3e18164cb00ec
nothiaki <celsohenrique367@gmail.com>
2024-09-15 12:56:23 -0300

    fix: some styles

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -2,6 +2,9 @@ return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
-    vim.cmd([[colorscheme gruvbox]])
+    vim.cmd([[
+      colorscheme gruvbox
+      hi SignColumn guibg=#282828
+    ]])
   end,
 }
